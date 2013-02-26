2bdccba98144373ce208dc0232dccced99110159
Chris McCormick <2059024M@student.gla.ac.uk>
2013-02-26 23:50:17 +0000

    http://www.laphroaig.com/

--- what_dram_project/populate.py
+++ what_dram_project/populate.py
@@ -1,3 +1,7 @@
 from dramapp.models import Region, Distillery, Whisky, Comments
 
+Region(region = 'Islay', views=0).save()
+
+Distillery(name = 'Laphroaig', latitude = '55.62989', longitude = '-6.15202', images = 'test', website = 'http://www.laphroaig.com/', tourevents = 'test',).save()
+
 
