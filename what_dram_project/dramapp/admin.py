9df703c7328c7944a146248ce7f90a2bd17e38d3
Eva Luckhiram <eluckhiram@gmail.com>
2013-02-26 17:39:46 +0000

    user auth

--- what_dram_project/dramapp/admin.py
+++ what_dram_project/dramapp/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from dramapp.models import Region, Distillery, Whisky, Member, Comments
+from dramapp.models import Region, Distillery, Whisky, Comments
 from dramapp.models import UserProfile
 
 admin.site.register(Region)
 admin.site.register(Distillery)
 admin.site.register(Whisky)
-admin.site.register(Member)
 admin.site.register(Comments)
 admin.site.register(UserProfile)
 
