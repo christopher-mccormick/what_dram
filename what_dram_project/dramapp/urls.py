b995d088e9976bd0731aee4f04c3f8e4f5dcfaea
Chris McCormick <2059024M@student.gla.ac.uk>
2013-02-28 21:43:00 +0000

    Map working, only with absolute sizes

--- what_dram_project/dramapp/urls.py
+++ what_dram_project/dramapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^register/$', views.register, name='register'),
         url(r'^whisky/$', views.whisky, name='whisky'),
-        url(r'^distillery/$', views.whisky, name='distillery'),
+        url(r'^distillery/$', views.distillery, name='distillery'),
 )
