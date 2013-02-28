0f4d7f5b5f82e4cab6828d7431c69870506e78cc
Chris McCormick <2059024M@student.gla.ac.uk>
2013-02-28 12:32:54 +0000

    login box

--- what_dram_project/static/javascripts/login.js
+++ what_dram_project/static/javascripts/login.js
@@ -1,27 +1,4 @@
-/*
-var mouse_is_inside = false;
 
-$(document).ready(function() {
-    $(".login_btn").click(function() {
-        var loginBox = $("#login_box");
-        if (loginBox.is(":visible"))
-            loginBox.fadeOut("fast");
-        else
-            loginBox.fadeIn("fast");
-        return false;
-    });
-    
-    $("#login_box").hover(function(){ 
-        mouse_is_inside=true; 
-    }, function(){ 
-        mouse_is_inside=false; 
-    });
-
-    $("body").click(function(){
-        if(! mouse_is_inside) $("#login_box").fadeOut("fast");
-    });
-});
-*/
 
 $(document).ready(function() {
     
@@ -33,4 +10,5 @@ $(document).ready(function() {
     function(){
         $("#login_box").fadeOut("slow");
         return false;
-    }); 
+    });
+});  
