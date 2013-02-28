d988ea901a1bff13e879f46df7d0e56bd5128e1f
Eva Luckhiram <eluckhiram@gmail.com>
2013-02-28 17:10:37 +0000

    java

--- what_dram_project/static/javascripts/login.js
+++ what_dram_project/static/javascripts/login.js
@@ -3,12 +3,10 @@
 $(document).ready(function() {
     
     
-    $(".login_btn").hover(function(){
-    $("#login_box").fadeIn("fast");
-    return true;
+    $(".login_btn, #login_box").hover(function(){
+    $("#login_box").show("fast");
     },
     function(){
         $("#login_box").fadeOut("slow");
-        return false;
     });
 });  
