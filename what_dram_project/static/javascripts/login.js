197ad0904e069bcad33d48691e1446d2bb763169
Chris McCormick <2059024M@student.gla.ac.uk>
2013-02-28 19:52:19 +0000

    still no working

--- what_dram_project/static/javascripts/login.js
+++ what_dram_project/static/javascripts/login.js
@@ -4,9 +4,19 @@ $(document).ready(function() {
     
     
     $(".login_btn, #login_box").hover(function(){
-    $("#login_box").show("fast");
+    $("#login_box").fadeIn("fast");
     },
     function(){
         $("#login_box").fadeOut("slow");
     });
-});  
+});
+
+//possible soultion??? nope worse
+// $(document).ready(function() {
+//     var b = $('.login_btn');
+//     $('#login_box').bind('mouseenter mouseleave', function(e) {    
+//        var check = ( e.type === 'mouseenter' ) ?
+//           ( b.stop(0).fadeIn("fast")  ) :
+//           ( b.stop(0).fadeOut("slow") ) ;
+//     });
+// });  
